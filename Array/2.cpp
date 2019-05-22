7928d10b8d8cba09ca31fdc81399f3695533d340
SinghVikram97 <vikram.bedi97@gmail.com>
2019-05-22 21:38:29 +0530

    Update 2.cpp

--- Array/2.cpp
+++ Array/2.cpp
@@ -3,41 +3,56 @@ Largest Unique Substring
 Given a string S,
 find the largest substring with no repetition i.e.largest substring which contain all unique characters.
 
-void findLargestUniqueSubstring(char input[], char output[])
-{
+// input - given string
+// output - output string that contains largest unique substring
+#include<bits/stdc++.h>
+using namespace std;
+void findLargestUniqueSubstring(char input[], char output[]){
 
-  string s(input);
-  int n = s.length();
-  string ans;
-
-  int i = 0;
-  int j = 1;
-
-  unordered_set<char> mySet;
-  mySet.insert(s[0]);
-  int maxLength = -1;
-  int length = 1;
-
-  while (i <= j && j < n)
-  {
-
-    if (mySet.find(s[j]) != mySet.end())
-    {
-      // Cut our window from length
-      auto it = mySet.find(s[j]);
-      mySet.erase(it);
-      i++;
-      length--;
+    string s(input);
+    int n=s.length();
+    
+    int i=0;
+    int j=1;
+    
+    unordered_set<char> mySet;
+    mySet.insert(s[0]);
+    int maxLength=-1;
+    int length=1;
+    int st;
+    int end;
+    int maxSt;
+    int maxEnd;
+    st=0;
+    end=0;
+    
+    while(i<=j && j<n){
+        
+        if(mySet.find(s[j])!=mySet.end()){
+            // Cut our window from length
+            auto it=mySet.find(s[j]);
+            mySet.erase(it);
+            i++;
+            length--;
+            st++;
+        }
+        else{
+            // Increase our window
+            mySet.insert(s[j]);
+            j++;
+            length++;
+            end++;
+            if(length>maxLength){
+                maxLength=length;
+                maxSt=st;
+                maxEnd=end;
+            }
+        }
     }
-    else
-    {
-      // Increase our window
-
-      mySet.insert(s[j]);
-      j++;
-      length++;
-      maxLength = max(maxLength, length);
+    string ans="";
+    for(int i=maxSt;i<=maxEnd;i++){
+        ans=ans+s[i];
     }
-  }
-  cout << maxLength << endl;
+
+    strcpy(output,ans.c_str());
 }
